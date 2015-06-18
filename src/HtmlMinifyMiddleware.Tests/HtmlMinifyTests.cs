fd02f5aba596c38c3a81d36e4a9e2e6c3aa02f13
Damian Hickey <dhickey@gmail.com>
2015-06-18 09:44:31 +0200

    Nearly working stream based html minification.

---
+++ src/HtmlMinifyMiddleware.Tests/HtmlMinifyTests.cs
@@ -0,0 +1,110 @@
+﻿namespace HtmlMinifyMiddleware.Tests
+{
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Microsoft.Owin;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class HtmlMinifyTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public HtmlMinifyTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void Blah()
+        {
+            // 1 Block copy array into buffer
+            // 2 Memory stream read
+            // 3 Move trailing bytes to front
+            // 4 Block copy array into buffer repeat
+
+            string s = new string('\u03D6', 2);
+            int bufferLength = 4096;
+
+            var encoding = Encoding.UTF8;
+            var bytes = encoding.GetBytes(s);
+            _testOutputHelper.WriteLine(bytes.Length.ToString());
+
+            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length-1);
+            _testOutputHelper.WriteLine(arraySegment.Count.ToString());
+
+            var memoryStream = new MemoryStream(arraySegment.Array, arraySegment.Offset, arraySegment.Count);
+            using(var reader = new StreamReader(memoryStream, Encoding.UTF8))
+            {
+                var read = reader.Read();
+                _testOutputHelper.WriteLine(read.ToString());
+                _testOutputHelper.WriteLine("MS POS " + memoryStream.Position);
+
+                /*char c = (char) read;
+                _testOutputHelper.WriteLine(c.ToString());*/
+
+                read = reader.Read();
+                _testOutputHelper.WriteLine(read.ToString());
+                _testOutputHelper.WriteLine("MS POS " + memoryStream.Position);
+
+                read = reader.Read();
+                _testOutputHelper.WriteLine(read.ToString());
+                _testOutputHelper.WriteLine("MS POS " + memoryStream.Position);
+            }
+
+            _testOutputHelper.WriteLine(s);
+        }
+
+        [Fact]
+        public async Task Should_minify_html()
+        {
+            var s = new string(' ', 10000);
+            string html = "   <html>" + Environment.NewLine + s + "</html>";
+            
+            var appfunc = HtmlMinify.Middleware(async env =>
+            {
+                var context = new OwinContext(env);
+                context.Response.ContentType = "text/html";
+                var bytes = Encoding.UTF8.GetBytes(html);
+                await context.Response.WriteAsync(bytes);
+            });
+            
+            var handler = new OwinHttpMessageHandler(appfunc);
+            using(var client = new HttpClient(handler))
+            {
+                var response = await client.GetAsync("http://localhost/");
+                var body = await response.Content.ReadAsStringAsync();
+
+                body.Should().Be(@"<html> </html>");
+            }
+        }
+
+        [Fact]
+        public async Task Should_not_minify_plain_text()
+        {
+            string html = @"   <html>
+                            </html>";
+
+            var appfunc = HtmlMinify.Middleware(async env =>
+            {
+                var context = new OwinContext(env);
+                context.Response.ContentType = "text/plain";
+                var bytes = Encoding.UTF8.GetBytes(html);
+                await context.Response.WriteAsync(bytes);
+            });
+
+            var handler = new OwinHttpMessageHandler(appfunc);
+            using (var client = new HttpClient(handler))
+            {
+                var response = await client.GetAsync("http://localhost/");
+                var body = await response.Content.ReadAsStringAsync();
+
+                body.Should().Be(html);
+            }
+        }
+    }
+}
---
+++ src/HtmlMinifyMiddleware/HtmlMinify.cs
@@ -0,0 +1,158 @@
+﻿
+
+namespace HtmlMinifyMiddleware
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Owin;
+    using MidFunc =
+        System.Func<System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>
+            , System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>>;
+
+    public static class HtmlMinify
+    {
+        public static MidFunc Middleware
+        {
+            get
+            {
+                return next => env =>
+                {
+                    var context = new OwinContext(env);
+
+                    context.Response.Body = new HtmlMinifyStream(context.Response.Body, () => context.Response.ContentType);
+
+                    return next(env);
+                };
+            }
+        }
+    }
+
+    internal class HtmlMinifyStream : Stream
+    {
+        private readonly Stream _inner;
+        private readonly Lazy<Action<byte[], int, int>> _lazyWrite;
+        private const char EmptySpace = ' ';
+
+        public HtmlMinifyStream(Stream inner, Func<string> getContentType)
+        {
+            _inner = inner;
+
+            _lazyWrite = new Lazy<Action<byte[], int, int>>(() =>
+            {
+                if (!string.Equals(getContentType(), "text/html", StringComparison.OrdinalIgnoreCase))
+                {
+                    return _inner.Write;
+                }
+
+                var inputBuffer = new byte[4096]; //4K + 4 bytes for trailing truncated UTF-8 char
+                var memoryStream = new MemoryStream(inputBuffer); // Should be pooled
+                int inputBufferOffset = 0;
+                var outputCharBuffer = new char[Encoding.UTF8.GetMaxCharCount(4096)];
+                var encoding = Encoding.UTF8;
+                int charPosition = 0;
+
+                //buffer length should not be greater than 4096
+                return ((buffer, offset, count) =>
+                {
+                    Buffer.BlockCopy(buffer, offset, inputBuffer, inputBufferOffset, count);
+
+                    using (var reader = new StreamReader(memoryStream, encoding, true, 4096, true))
+                    {
+                        char previous = EmptySpace;
+                        int read = reader.Read();
+
+                        while(read >= 0)
+                        {
+                            var c = (char) read;
+                            if (c == EmptySpace && (charPosition == 0 || previous == EmptySpace))
+                            {
+                                previous = c;
+                            }
+                            else if(c == '\r' || c == '\n')
+                            {
+                                previous = EmptySpace;
+                            }
+                            else
+                            {
+                                outputCharBuffer[charPosition] = c;
+                                previous = c;
+                                charPosition++;
+                            }
+                            read = reader.Read();
+                        }
+                    }
+
+                    int remainingBytes = inputBuffer.Length - (int)memoryStream.Position;
+                    Buffer.BlockCopy(inputBuffer, (int)memoryStream.Position, inputBuffer, 0, remainingBytes);
+
+                    var outputBytes = encoding.GetBytes(outputCharBuffer, 0, charPosition);
+                    charPosition = 0;
+                    _inner.Write(outputBytes, 0, outputBytes.Length);
+                });
+            });
+        }
+
+        public override void Flush()
+        {
+            _inner.Flush();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return _inner.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            _inner.SetLength(value);
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return _inner.Read(buffer, offset, count);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            int pageSize = 4096;
+
+            while (count > pageSize)
+            {
+                _lazyWrite.Value(buffer, offset, pageSize);
+                count -= pageSize;
+                offset += pageSize;
+            }
+            _lazyWrite.Value(buffer, offset, count);
+        }
+
+        public override bool CanRead
+        {
+            get { return _inner.CanRead; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return _inner.CanSeek; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return _inner.CanWrite; }
+        }
+
+        public override long Length
+        {
+            get { return _inner.Length; }
+        }
+
+        public override long Position
+        {
+            get { return _inner.Position; }
+            set { _inner.Position = value; }
+        }
+    }
+}
